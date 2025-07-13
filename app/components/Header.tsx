703b95238a6ae1240589cbe40e328ced82373eff
Diego Carmona <diegoncarmona@gmail.com>
2025-07-13 00:02:49 -0300

    add user ingredients to list and display the list

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,9 @@
 
 export default function Header() {
   return (
-    <header className="bg-white text-white flex items-center justify-center h-24 shadow-md z-10 relative">
+    <header className="bg-white text-white flex items-center justify-center h-24 shadow-sm z-10 relative">
         <img className="h-14" src="/chef-gpt-icon.png" alt="" />
-        <h1 className="text-gray-900 text-3xl pl-3 font-normal tracking-tighter">Chef GPT</h1>
+        <h1 className="text-stone-950 text-3xl pl-3 font-normal tracking-tighter">Chef GPT</h1>
     </header>
   );
 }
--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,24 +1,53 @@
 "use client";
 
+import React from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState<string[]>([]);
+
+  const ingredientList = ingredients.map((ingredient) => (
+    <li key={ingredient} className="break-words">
+      {ingredient}
+    </li>
+  ));
+
+  function addIngredient(formData: FormData) {
+    const ingredient = formData.get("ingredient") as string;
+    setIngredients((prevIngredients) => [...prevIngredients, ingredient]);
+  
+  }
+
   return (
-    <main className="bg-stone-100 min-h-[calc(100vh-6rem)]">
+    <main className="bg-stone-50 min-h-[calc(100vh-6rem)]">
       <form
-        action=""
-        className="pt-10 gap-2 w-full px-16 flex justify-center"
+        action={addIngredient}
+        className="pt-18 gap-3 px-5 flex justify-center mx-auto"
       >
         <input
           type="text"
+          name="ingredient"
+          maxLength={50}
           className="h-10 bg-white rounded p-3 w-[400px] rounded-lg border border-stone-400 shadow-sm focus:outline-none focus:border-stone-500 "
-          placeholder="ex. orégano"
+          placeholder="ex. atum"
+          aria-label="Digite um ingrediente"
         />
         <button
           type="submit"
-          className="bg-black text-white text-sm font-normal py-2 px-12 rounded-lg"
+          className="bg-stone-950 text-white text-sm font-medium py-2 px-12 rounded-lg flex items-center whitespace-nowrap w-[]"
         >
-          Add ingredient
+          + Adicionar
         </button>
       </form>
+      {ingredients.length > 0 && (
+        <section className="mt-10 mx-auto flex flex-col max-w-[585px] px-5">
+           <h2 className="text-3xl font-semibold text-stone-950 mb-4">
+          Ingredients on hand:
+          </h2>
+        <ul className="list-disc list-inside max-w-148 break-all">
+          {ingredientList}
+        </ul>
+        </section>
+      )}
     </main>
   );
 }
